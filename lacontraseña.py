33d55af9ffdcbff9d9c89243b535d8ed4dd522c8
BrailyRS03004 <78428750+BrailyRS03004@users.noreply.github.com>
2022-08-06 12:08:23 -0400

    Add files via upload

---
+++ lacontraseña.py
@@ -0,0 +1,17 @@
+# contraseña = int(input(print("Introduzca la contraseña: ")))
+
+# if contraseña == 29043:
+#  print("Contraseña correcta. ")
+# print("Felicidades ")
+# else:
+#   print("Contraseña incorrecta. ")
+#  print("Intentalo denuevo  ")
+
+#Es diferente per aun asi el mio funciona
+
+key = "contraseña"
+password = input("Introduce la contraseña: ")
+if key == password.lower():
+    print("La contaseña coincide")
+else:
+    print("La contraseña no coincide")
